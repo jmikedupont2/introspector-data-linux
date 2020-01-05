7b7438eb3f62a1b1dec9fbdb97ab5712487f98f3
j. mike dupont <jmikedupont2@gmail.com>
2020-01-05 18:17:12 -0500

    setup

--- tests/test_run_processing.py
+++ tests/test_run_processing.py
@@ -3,4 +3,4 @@ import os
 
 
 def test_main():
-    print(os.system("ls builtin-trace.i.001t.json"))
+    print(os.system("python3 -m gcc.tree.gcc-tu-reader-json-process introspector/projects/linux/tools/perf/builtin-trace.i.001t.json"))
