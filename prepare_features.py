019a6bc51e46ae3bf6dcdfec695eb73ab0af1976
james miked dupont <jmikedupont2@gmail.com>
2020-06-26 07:27:35 -0400

    preparing features for nodes
    This version creates multiple test cases where we prepare test data by
    expanding the graph by a fixed number of layers. Each node is recursed
    N number of times to look at other nodes.
    
    Next steps could be to count the number of incoming fields and types,
    also to split out tables of different types/morphs/forms.
    
    We want to try and train a netork to look at each field and see what
    other field is significant to it, where there is a correlation.

--- prepare_features.py
+++ prepare_features.py
@@ -1,5 +1,8 @@
+
 import json
 import pprint
+from flatten_dict import flatten
+
 
 identifier_field = "_id"
 type_field = "_type"
@@ -8,6 +11,9 @@ known= [identifier_field,type_field]
 
 # determine the relationship between rows
 id_to_type = {}
+
+# extract the data for each type
+id_to_data = {}
 
 data = {}
 
@@ -18,18 +24,33 @@ def calculate_types(_data, level_name, target_level):
         ftype = {}
         type_name = row[type_field]
 
+        row2 = {}
+        
         for k in row:
-            if k in known:
-                continue# skip
             v = row[k]
-
-            if isinstance(v,str):
-                if v in _data:
-                    ftype[k] = id_to_type[v][level_name]
-                else:
-                    ftype[k] = "string"
+            if k in known:
+                row2[k] = row[k]
             else:
-                ftype[k] = str(type(v))
+                if isinstance(v,str):
+                    if v in _data:
+                        ftype[k] = id_to_type[v][level_name]
+                        row2[k] = id_to_data[v][level_name]
+                    else:
+                        ftype[k] = "string"
+                        row2[k]= row[k]
+                elif isinstance(v,int):
+                    ftype[k] = "pythonint"
+                elif isinstance(v,dict):
+                    if "name" in v:
+                        ftype[k] = "named_node"
+                    else:
+                        pprint.pprint(v)
+                        raise Exception(v)
+                        ftype[k] = "dict"
+                else:
+                    raise Exception(str(type(v)))
+                    ftype[k] = str(type(v))
+                    row2[k]= row[k]
 
         type_string = { type_name : ftype }
                 
@@ -37,6 +58,11 @@ def calculate_types(_data, level_name, target_level):
             id_to_type[_id] = { target_level : type_string }
         else:
             id_to_type[_id][target_level] = type_string
+            
+        if _id not in id_to_data:
+            id_to_data[_id] = { target_level : row2 }
+        else:
+            id_to_data[_id][target_level] = row2
 
 
 # read in the file and copy the data and create the first type layer
@@ -48,24 +74,45 @@ with open("linux_clean_formatted_compact.json") as fi:
         # save the row for second pass
         data[_id] = dict(row)        
         # remove all known fields
-        for k in known:
-            del row[k]            
-        fields =list(row.keys())
+
+        #for k in known:
+        #    del row[k]            
+        fields ="_".join(list(sorted(row.keys())))
         type_string = { type_name : fields }
         id_to_type[_id]  = { "level_1" : type_string }
+        
+        id_to_data[_id]  = { "level_1" : row }        
 
 
 calculate_types(data, level_name="level_1", target_level="level_2")
 calculate_types(data, level_name="level_2", target_level="level_3")
 calculate_types(data, level_name="level_3", target_level="level_4")
-
+def underscore_reducer(k1, k2):
+    if k1 is None:
+        return k2
+    else:
+        return k1 + "_" + k2
         
 # given a row of one type, what is the maximum number of fields it will have, how many occur togeter.
 
 #pprint.pprint(id_to_type)
 for x in id_to_type:
     for l in id_to_type[x]:
-        v = id_to_type[x][l]
-        print(json.dumps(v))
+
+        tv = id_to_type[x][l]
+        v = id_to_data[x][l]
+        v['_level'] = l
+
+        flatten_type = flatten(tv, reducer=underscore_reducer)
+
+        ordered = sorted(list(flatten_type.keys()))
+        typestring =  "_".join([
+            k + str(flatten_type[k]) for k in ordered
+        ])
+
+        v['_type'] = typestring
+        flatten_data = flatten(v, reducer=underscore_reducer)
+                
+        print(json.dumps(flatten_data))
 
 # python3 prepare_features.py | sort | uniq -c  |sort -n > common_types.txt
